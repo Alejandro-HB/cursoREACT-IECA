f8e59dcbd09572e6eb1db2643fa6e97d1c52a7d4
Alejandro Hernández Baca <a.hernandezbaca@ugto.mx>
2023-06-17 13:18:24 -0600

    clase 3 terminada

---
+++ 03-react/RCA/project1/product-app/src/components/ProductItem.jsx
@@ -0,0 +1,13 @@
+const ProductItem=({product})=>{
+    //console.log(props);
+    const {name, description, price}=product;
+    return (
+        <div className={`p-4 bg-slate-200 rounded-xl w-[150px] ${product.price<500 ? "bg-yellow-500":"bg-slate-200"}`}>
+            <h2>{name}</h2>
+            <p>{description}</p>
+            <p>${price}</p>            
+        </div>
+    );
+}
+
+export default ProductItem;
